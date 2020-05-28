42a043c79c451d8f4d977c50e7927b5ecd1bad0f
Daiana Szwimer <daiana.szwimer@prosoftwarefactory.com.ar>
2020-05-28 17:24:58 -0300

    Updates

---
+++ precache-manifest.4e16b3bda451c2d4c623fc2f67ef91c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f009a0730f50a0122861fb9d350a96",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fb8a7a18a458b75b410d",
+    "url": "/portfolio/static/css/main.5797eb9e.chunk.css"
+  },
+  {
+    "revision": "44c5a8aace941a8a8531",
+    "url": "/portfolio/static/js/2.4d2e063b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.4d2e063b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8a7a18a458b75b410d",
+    "url": "/portfolio/static/js/main.5637d276.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
